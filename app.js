0945d6abec587760d78e2edd7ef4527f8ae21e28
Casey Tritt <caseytritt@Caseys-MacBook-Pro.local>
2018-10-14 09:20:00 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log('using app.js ...');
+console.log('It works ...');
+//string
+//number (int, float)
+//boolean
+//assign types
+var myName;
+var hasHobbies;
+var myAge;
+var hobbies = ["development", "music", "travel"];
+console.log(hobbies);
---
+++ app.ts
@@ -0,0 +1,13 @@
+console.log('using app.js ...');
+console.log('It works ...');
+
+//string
+//number (int, float)
+//boolean
+
+//assign types
+let myName: string;
+let hasHobbies: boolean;
+let myAge: number;
+let hobbies: string[] = ["development", "music", "travel"];
+console.log(hobbies);
